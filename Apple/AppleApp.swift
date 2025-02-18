e3619016e2c934f2f9b2a48170db5c5c11b69f27
Ben Fowler <voxhavoccanis@gmail.com>
2025-02-18 15:32:41 +0800

    Initial Commit

---
+++ Apple/AppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleApp.swift
+//  Apple
+//
+//  Created by Ben Fowler on 18/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleTests/AppleTests.swift
@@ -0,0 +1,16 @@
+//
+//  AppleTests.swift
+//  AppleTests
+//
+//  Created by Ben Fowler on 18/2/2025.
+//
+
+import Testing
+
+struct AppleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
